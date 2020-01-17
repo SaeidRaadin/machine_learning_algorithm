f8a73c2593eb2f82d6e447ad21f0b4f71e1040aa
sirojnurulum <sirojnurulum@gmail.com>
2020-01-17 10:53:37 +0700

    fix find-s algorithm

--- find_s.py
+++ find_s.py
@@ -1,23 +1,33 @@
 import pandas as pd
 
+
+def find_s(datasets, init_hypotesis):
+    for x, y in datasets.iterrows():
+        for z in datasets.columns:
+            if init_hypotesis[z] != '?':
+                if init_hypotesis[z] != y[z]:
+                    init_hypotesis[z] = '?'
+    return init_hypotesis
+
+
+# import dataset into pandas dataframe
 datasets = pd.read_csv('datasets/enjoy_sport.csv')
 print(datasets)
 print('--------------------------------------------------------------\n')
 
+# remove any samples with 'no' target
 data = datasets[datasets.enjoy_sport != 'no']
 print(data)
 print('\n')
+# drop target atribute
 data = data.drop(['enjoy_sport'], axis=1)
+# initialize hypotesis
 h = data.loc[0]
 print(h)
 print('\n')
+
 data = data.drop([0], axis=0)
 print(data)
 print('\n')
-for x, y in data.iterrows():
-    for z in data.columns:
-        if h[z] != '?':
-            if h[z] != y[z]:
-                h[z] = '?'
 
-print(h)
+print(find_s(data, h))
